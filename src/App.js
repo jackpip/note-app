3a491c1987156b67d2fa6989a21c0accb7e2081f
Jack Pipitone <pipitone.jack@gmail.com>
2017-05-15 14:00:35 -0400

    Tag form submission

--- src/App.js
+++ src/App.js
@@ -61,6 +61,16 @@ class App extends Component {
     this.setState({ newTag: true });
   }
 
+  closeTagForm = () => {
+    this.setState({ newTag: false })
+  }
+
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId))
+    .catch((err) => console.log(err.response.data));
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -72,8 +82,10 @@ class App extends Component {
           <Note
             note={note}
             submitNote={this.submitNote}
-            showTagForm={this.showTagForm}
             newTag={newTag}
+            showTagForm={this.showTagForm}
+            closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
           />
           :
           <List
